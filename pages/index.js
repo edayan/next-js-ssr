0bac86375f9f37842dc79982f7f08d15e461cfa5
Saju Paul <saju.christel@blu-pa.com>
2020-04-04 17:11:37 +0530

    Adding data to the component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
+import React from 'react';
 import './index.css';
-import Card from  './Card';
+import Card from './Card';
+import data from '../data/data.json';
 
-export default () => {
-    return <div className="App">
-        <header className="App-header">
-            <img src="/static/logo.png" className="static-logo" alt="logo"/>
-        </header>
-        <div className="Grid">
-            <Card/>
-            <Card/>
-            <Card/>
+export default class Index extends React.Component {
+
+    static async getInitialProps() {
+        return {
+            cards: data
+        }
+    }
+
+    render() {
+        return <div className="App">
+            <header className="App-header">
+                <img src="/static/logo.png" className="static-logo" alt="logo" />
+            </header>
+            <div className="Grid">
+                {this.props.cards.map(card =>  <Card key={card.id} />)}
+            </div>
         </div>
-    </div>
+    }
+
 }
